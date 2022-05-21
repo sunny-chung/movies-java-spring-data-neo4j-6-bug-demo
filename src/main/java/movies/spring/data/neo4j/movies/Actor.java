79190c84f53e133f5848e83f57dba88b26881d2b
Sunny Chung <sunnychung@live.hk>
2022-05-21 17:47:22 +0800

    update spring boot version to 2.7.0

--- src/main/java/movies/spring/data/neo4j/movies/Actor.java
+++ src/main/java/movies/spring/data/neo4j/movies/Actor.java
@@ -3,6 +3,8 @@ package movies.spring.data.neo4j.movies;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
 import org.springframework.data.neo4j.core.schema.TargetNode;
 
@@ -11,6 +13,9 @@ import org.springframework.data.neo4j.core.schema.TargetNode;
  */
 @RelationshipProperties
 public class Actor {
+	@Id
+	@GeneratedValue
+	Long id;
 
 	@TargetNode
 	private final Person person;
